75bc7ab6ae281dd5e1649cd68b7a85a4636abb61
zifo <midoshaban088@gmail.com>
2025-02-13 01:02:37 +0200

    refactor: remove local host from wsgi file

--- wsgi.py
+++ wsgi.py
@@ -7,9 +7,3 @@ app = FastAPI(title="Quran APIs", version="0.1.0")
 
 # Include search router
 app.include_router(quran_search, prefix="/search", tags=["search"])
-
-if __name__ == "__main__":
-    import uvicorn
-
-    # Run the FastAPI app
-    uvicorn.run(app, host="127.0.0.1", port=8000)
